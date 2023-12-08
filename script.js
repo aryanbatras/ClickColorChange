8326a91b2668951d144d270789911c58eea0c6ae
Aryan Batra <135307006+aryanbatras@users.noreply.github.com>
2023-12-08 06:41:39 +0530

    auto feature

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const body = document.body;
-const allbtn = document.querySelectorAll("button");
+const btn = document.querySelector("button");
+const auto = btn.nextElementSibling;
+let id;
 
 function randomColor(){
 	const red = Math.floor(Math.random() * 256);
 	const green = Math.floor(Math.random() * 256);
 	const blue = Math.floor(Math.random() * 256);
-	
 	return `rgb(${red},${green},${blue})`
 }
 
-allbtn.forEach(btn => {
-	
+btn.addEventListener("click", function(){
+	const random = randomColor();
+	body.style.backgroundColor = random;
+});
 
-	btn.addEventListener("click", function(){
+auto.addEventListener("click", function(e){
+	if(e.target.textContent === " Auto: ON "){
+		e.target.textContent = " Auto: OFF ";
+		clearTimeout(id);
+	}
+	else if(e.target.textContent === " Auto: OFF "){
+		e.target.textContent = " Auto: ON ";
 		
+		const time = range.value * 1000;
+		id = setInterval(() => {
+			const random = randomColor();
+			body.style.backgroundColor = random;
+		}, time);
+	}
+});
+
+const range = document.querySelector("input");
+
+range.addEventListener("change", e => {
+	e.target.previousElementSibling.textContent = `${range.value}s`;
+	
+	if(auto.textContent === " Auto: ON "){
+	clearTimeout(id);
+	const time = range.value * 1000;
+	id = setInterval(() => {
 		const random = randomColor();
 		body.style.backgroundColor = random;
-
-	});
-
+	}, time);
+	}
 });
